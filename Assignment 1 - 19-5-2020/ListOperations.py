2422c4ac0c0d411f5be3d724efb20355cc06f12a
Vinod30 <63861869+Vinod30@users.noreply.github.com>
2020-05-19 13:20:06 +0530

    Add files via upload
    New file added

---
+++ Assignment 1 - 19-5-2020/ListOperations.py
@@ -0,0 +1,20 @@
+list=[2,6,1,9,6,3,7,4,6]
+
+a = list.count(6)
+print(a)
+
+b = list.index(9)
+print(b)
+
+list.reverse()
+print(list)
+
+list.append(777)
+print(list)
+
+list.sort()
+print(list)
+
+
+list.pop()
+print(list)
